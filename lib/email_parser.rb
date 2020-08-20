49b9045d737986a103cffa8660656e54f7e4fc36
Javier Aguilar <coyote176@msn.com>
2020-08-20 02:42:06 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -15,3 +15,14 @@ class EmailAddressParser
    end  
 end
 
+class EmailAddressParser
+  attr_accessor :email_addresses
+
+  def initialize(email_addresses)
+    @email_addresses = email_addresses
+  end
+
+  def parse
+    @email_addresses = email_addresses.split(/, | /).uniq
+  end
+end
